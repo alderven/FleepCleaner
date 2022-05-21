bd4e592ec7e0c587c6f77961f260cf83741028e2
alderven <alderven@gmail.com>
2022-05-21 10:41:25 +0300

    Download History file automatically

--- fleep_cleaner.py
+++ fleep_cleaner.py
@@ -1,4 +1,7 @@
+import os
 import json
+import shutil
+import zipfile
 import argparse
 import requests
 
@@ -68,6 +71,28 @@ def auth(email, password):
     return ticket_id, token_id
 
 
+def sync(token_id, ticket_id):
+    """ Get account info
+    :param token_id: Fleep Token Id
+    :param ticket_id: Fleep Ticked Id
+    :return response body """
+    r = requests.post('https://fleep.io/api/alias/sync', cookies={'token_id': token_id}, json={'ticket': ticket_id})
+    assert r.status_code == 200, r.text
+    return r.json()
+
+
+def download(url, output_file, token_id, ticket_id):
+    """ Download file
+    :param url: File URL
+    :param output_file: Output file path
+    :param token_id: Fleep Token Id
+    :param ticket_id: Fleep Ticked Id """
+    response = requests.get(url, stream=True, cookies={'token_id': token_id}, json={'ticket': ticket_id})
+    with open(output_file, 'wb') as f:
+        shutil.copyfileobj(response.raw, f)
+    del response
+
+
 def delete_file(conversation_id, token_id, ticket_id, message_nr, attachment_id):
     """ Delete file from Fleep
     :param conversation_id: Fleep Conversation Id
@@ -79,11 +104,10 @@ def delete_file(conversation_id, token_id, ticket_id, message_nr, attachment_id)
     assert r.status_code == 200, r.text
 
 
-def main(email, password, file_path, file_size, extension):
+def main(email, password, file_size, extension):
     """ Remove files from Fleep with defined file size
     :param email: Fleep email
     :param password: Fleep password
-    :param file_path: Path to json file
     :param file_size: File size in MB
     :param extension: File extension """
 
@@ -91,12 +115,24 @@ def main(email, password, file_path, file_size, extension):
     print('1. Auth to Fleep')
     ticket_id, token_id = auth(email, password)
 
-    # 2. Parse json  file
-    print('2. Parse json file')
-    files = parse_file(file_path)
+    # 2. Download History file
+    print('2. Download History file')
+    r = sync(token_id, ticket_id)
+    export_file = json.loads(r['stream'][0]['export_files'][0])
+    file_name = export_file['file_name'][:-4]
+    output_file = f"{export_file['file_id']}.zip"
+    if not os.path.isfile(output_file):
+        url = 'https://fleep.io' + export_file['file_url']
+        download(url, output_file, token_id, ticket_id)
+        with zipfile.ZipFile(output_file, 'r') as zip_ref:
+            zip_ref.extractall()
 
-    # 3. Find files to delete
-    print('3. Find files to delete')
+    # 3. Parse History  file
+    print('3. Parse History file')
+    files = parse_file(file_name)
+
+    # 4. Find files to delete
+    print('4. Find files to delete')
     i = 0
     size = 0
     files_selected = []
@@ -110,7 +146,7 @@ def main(email, password, file_path, file_size, extension):
         print('Files not found! Exit script')
         return
 
-    # 4, Delete files
+    # 5. Delete files
     print(f'Files found: {i}. Total files size: {int(size)} MB.')
     while True:
         action = input('Delete files? Type "y" to delete, type "n" to abort: ')
@@ -122,8 +158,8 @@ def main(email, password, file_path, file_size, extension):
         else:
             print(f'Unknown operation: "{action}".')
 
-    # 4. Delete files
-    print('4. Delete files')
+    # 6. Delete files
+    print('6. Delete files')
     for file in files_selected:
         print(f'Delete file [{file["size"]} MB] — {file["url"]}')
         delete_file(conversation_id=file['conversation_id'], token_id=token_id, ticket_id=ticket_id, message_nr=file['message_nr'], attachment_id=file['attachment_id'])
@@ -134,8 +170,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Fleep Cleaner')
     parser.add_argument('-e', '--email', help='Fleep email', required=True)
     parser.add_argument('-p', '--password', help='Fleep password', required=True)
-    parser.add_argument('-f', '--file', help='Path to exported json file', required=True)
     parser.add_argument('-s', '--size', help='Filter files by size in MB', required=False, type=int, default=0)
     parser.add_argument('-ex', '--extension', help='Filter files by extension', required=False, default='')
     args = parser.parse_args()
-    main(args.email, args.password, args.file, args.size, args.extension)
+    main(args.email, args.password, args.size, args.extension)
